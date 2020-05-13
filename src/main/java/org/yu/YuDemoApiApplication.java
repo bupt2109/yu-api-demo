76ac3581902d156959f2c4b17efd4763af5a11c1
lyx <565865732@qq.com>
2020-05-13 11:24:40 +0800

    create a spring boot project by idea

---
+++ src/main/java/org/yu/YuDemoApiApplication.java
@@ -0,0 +1,13 @@
+package org.yu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuDemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YuDemoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/yu/YuDemoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuDemoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
